85efedc7170c65521294a537df6f71229b201556
victor meneghini <vhmeneghini@gmail.com>
2022-01-05 12:08:34 -0300

    feat: inject emailvalidtor dependency

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => {
+  console.log(error)
   return {
     statusCode: 400,
     body: error
