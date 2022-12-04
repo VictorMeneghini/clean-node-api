73e3f23d9d104a62ed8511b477c377df49fb6458
Victor Meneghini <vhmeneghini@gmail.com>
2022-12-04 10:38:33 +0000

    refactor: move map helper to Mongo-helper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,7 +8,6 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const [account] = result.ops
-    const { _id, ...formatedAccount } = account
-    return { ...formatedAccount, id: _id }
+    return MongoHelper.map(account)
   }
 }
