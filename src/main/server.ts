704462082443835b613479fd059d74188e07ee42
Victor Meneghini <vhmeneghini@gmail.com>
2022-12-04 17:23:57 +0000

    refactor: create env to handle paths

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(5000, () => console.log('server running on http://localhost:5000'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`server running on http://localhost:${env.port}`))
+  })
+  .catch(console.error)
